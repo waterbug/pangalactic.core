e3ce63db03c662734a14beb6cb4feb98c01519e5
Steve Waterbury <waterbug@pangalactic.us>
2020-01-17 17:07:21 -0500

    [WIP] more work on mel util.

--- pangalactic/core/utils/mel.py
+++ pangalactic/core/utils/mel.py
@@ -15,39 +15,28 @@ mel_fields = OrderedDict(
     row=dict(range='int', name='Row in MEL Report'),
     level=dict(range='int', name='Level in Assembly Hierarchy'),
     name=dict(range='str', name='System/Subsystem Name'),
-    unit_mass=dict(range='float', name='Unit Mass'),
+    m_unit=dict(range='float', name='Unit Mass'),
     cold_units=dict(range='int', name='Cold (Unpowered) Units'),
     hot_units=dict(range='int', name='Hot (Powered) Units'),
-    fhw_units=dict(range='int', name='Flight Units'),
-    fhw_spares=dict(range='int', name='Flight Spares'),
+    flight_units=dict(range='int', name='Flight Units'),  # cold + hot
+    flight_spares=dict(range='int', name='Flight Spares'),
     etu_qual_units=dict(range='int', name='ETU/Qual Units'),
-    em_edu_prototype_units=dict(range='int',
-                                name='Units Used in Prototype'),
-    mass_cbe=dict(range='float', name='Mass CBE'),
-    mass_ctgcy=dict(range='float', name='Mass Contingency'),
-    mass_mev=dict(range='float', name='Mass MEV'),
-    nom_unit_power_cbe=dict(range='float',
-                            name='Nominal Unit Power CBE'),
-    nom_power_cbe=dict(range='float',
-                            name='Nominal Power CBE'),
-    nom_power_ctgcy=dict(range='float',
-                            name='Nominal Power Contingency'),
-    nom_power_mev=dict(range='float',
-                            name='Nominal Power MEV'),
-    peak_unit_power_cbe=dict(range='float',
-                            name='Peak Unit Power CBE'),
-    peak_total_power_cbe=dict(range='float',
-                            name='Peak Power CBE'),
-    peak_power_ctgcy=dict(range='float',
-                            name='Peak Power Contingency'),
-    peak_power_mev=dict(range='float',
-                            name='Peak Power MEV'),
-    quiescent_total_power=dict(range='float',
-                            name='Quiescent Power CBE'),
+    em_edu_prototype_units=dict(range='int', name='Units Used in Prototype'),
+    m_cbe=dict(range='float', name='Mass CBE'),
+    m_ctgcy=dict(range='float', name='Mass Contingency'),
+    m_mev=dict(range='float', name='Mass MEV'),
+    nom_p_unit_cbe=dict(range='float', name='Nominal Unit Power CBE'),
+    nom_p_cbe=dict(range='float', name='Nominal Power CBE'),
+    nom_p_ctgcy=dict(range='float', name='Nominal Power Contingency'),
+    nom_p_mev=dict(range='float', name='Nominal Power MEV'),
+    peak_p_unit_cbe=dict(range='float', name='Peak Unit Power CBE'),
+    peak_p_cbe=dict(range='float', name='Peak Power CBE'),
+    peak_p_ctgcy=dict(range='float', name='Peak Power Contingency'),
+    peak_p_mev=dict(range='float', name='Peak Power MEV'),
+    quiescent_p_cbe=dict(range='float', name='Quiescent Power CBE'),
     quoted_unit_price=dict(range='float', name='Quoted Unit Price ($K)'),
     composition=dict(range='str', name='Composition'),
-    additional_information=dict(range='str',
-                            name='Additional Information'),
+    additional_information=dict(range='str', name='Additional Information'),
     trl=dict(range='int', name='Technology Readiness Level (TRL)'),
     similarity=dict(range='str', name='Similarity to Existing'),
     heritage_design=dict(range='str', name='Heritage Design'),
@@ -72,6 +61,10 @@ mel_fields = OrderedDict(
                             name='Engineering Complexity Mod. Level'),
     )
 
+# 'field_names' will have same ordering as schema, which is an ordered dict --
+# the 'field_names' list is needed to maintain the field ordering when
+# serialized/deserialized, since ordinary dicts do not maintain ordering when
+# deserialized but lists do.
 mel_schema = dict(field_names=list(mel_fields), fields=mel_fields)
 
 def read_mel(orb, context_id):
@@ -94,61 +87,76 @@ def read_mel(orb, context_id):
         f.close()
     return mel
 
-def refresh_mel(mel, context, is_project=True):
+def refresh_mel(mel, context):
     """
-    Refresh the generated fields in a Master Equipment List (MEL)
+    Refresh the generated fields in a Master Equipment List (MEL), or if no MEL
+    is passed in, generate a new one.  The MEL object is a dict that maps oids
+    to MEL "records", where the record structure is specified by the mel
+    schema.
+
+        mel[oid] = [row, level, name, unit_mass, ...]
 
     Args:
-        mel (list of dict):  the MEL to be refreshed
+        mel (dict of dicts):  the MEL to be refreshed
         context (Project or Product):  the project or system
-        is_project (bool):  True if context is a Project; False otherwise
     """
-    start_row = 1
-    if is_project:
-        # context is Project, so may include several systems
-        project = context
-        # HEADER
-        # mel_label = 'MISSION: {}'.format(project.id)
-        # worksheet.write(hrow1, 1, mel_label, fmts['left_pale_blue_bold_12'])
-        system_names = [psu.system.name.lower() for psu in project.systems]
-        system_names.sort()
-        systems_by_name = {psu.system.name.lower() : psu.system
-                           for psu in project.systems}
-        for system_name in system_names:
-            last_row = get_components_parms(mel, 1, start_row,
-                                            systems_by_name[system_name])
-            start_row = last_row + 1
+    mel = mel or {}
+    new = False
+    if not mel:
+        # TODO:  use read_mel() to look for existing MEL related to 'context'
+        new = True
+        row = 1
+    if new:
+        if isinstance(context, orb.classes['Project']):
+            # context is Project, so may include several systems
+            project = context
+            system_names = [psu.system.name.lower() for psu in project.systems]
+            system_names.sort()
+            systems_by_name = {psu.system.name.lower() : psu.system
+                               for psu in project.systems}
+            for system_name in system_names:
+                system = systems_by_name[system_name]
+                row = get_components_parms(mel, 1, row, system)
+        elif isinstance(context, orb.classes['Product']):
+            # context is Product -> a single system MEL
+            system = context
+            row = get_components_parms(mel, 1, row, system)
+        else:
+            # not a Project or Product -- yikes, punt!
+            mel = {}
     else:
-        # context is Product -> a single system MEL
-        system = context
-        # ANOTHER HEADER
-        # worksheet.write(hrow1, 1, system.name, fmts['left_pale_blue_bold_12'])
-        get_components_parms(mel, 1, start_row, system)
+        # existing MEL:
+        #     [1] add/remove oids as necessary
+        #     [2] update existing oids
+        mel = {}
+    return mel
 
 def get_components_parms(mel, level, row, component, qty=1):
-    mcbe = get_pval(orb, component.oid, 'm[CBE]')
-    ctgcy_m = str(100 * get_pval(orb, component.oid, 'm[Ctgcy]')) + ' %'
-    mmev = get_pval(orb, component.oid, 'm[MEV]')
-    pcbe = get_pval(orb, component.oid, 'P[CBE]')
-    ctgcy_P = str(100 * get_pval(orb, component.oid, 'P[Ctgcy]')) + ' %'
-    pmev = get_pval(orb, component.oid, 'P[MEV]')
-    # columns:
+    oid = component.oid
+    mel['m_unit'] = get_pval(orb, oid, 'm[CBE]')
+    mel['m_cbe'] = qty * mel['m_unit']
+    mel['m_ctgcy'] = get_pval(orb, oid, 'm[Ctgcy]')
+    mel['m_mev'] = qty * get_pval(orb, oid, 'm[MEV]')
+    mel['nom_p_unit_cbe'] = get_pval(orb, oid, 'P[CBE]')
+    mel['nom_p_cbe'] = qty * mel['nom_p_unit_cbe']
+    mel['nom_p_ctgcy'] = get_pval(orb, oid, 'P[Ctgcy]')
+    mel['nom_p_mev'] = qty * get_pval(orb, oid, 'P[MEV]')
+    # columns in spreadsheet MEL:
     #   0: Level
     #   1: Name
-    #   2: UNIT MASS CBE
-    #   8: Mass CBE
-    #   9: Mass Contingency (Margin)
-    #  10: Mass MEV
-    #  12: Power CBE
-    #  13: Power Contingency (Margin)
-    #  14: Power MEV
+    #   2: Unit MASS CBE
+    #   9: Mass CBE
+    #  10: Mass Contingency (%)
+    #  11: Mass MEV
+    #  12: Unit Power CBE
+    #  13: Power CBE
+    #  14: Power Contingency (%)
+    #  15: Power MEV
     row += 1
     print('writing {} in row {}'.format(component.name, row))
     # then write the "LEVEL" cell
-    mel[row]['level'] = level
-    # level-based indentation
-    spaces = '  ' * level
-    mel[row]['Name'] = spaces + component.name
+    mel[oid]['level'] = level
+    mel[oid]['name'] = component.name
     if component.components:
         next_level = level + 1
         comp_names = [acu.component.name.lower()
@@ -159,8 +167,8 @@ def get_components_parms(mel, level, row, component, qty=1):
         qty_by_name = {acu.component.name.lower() : acu.quantity or 1
                        for acu in component.components}
         for comp_name in comp_names:
-            mel[row] = get_components_parms(mel, next_level, row,
-                                            comps_by_name[comp_name],
-                                            qty=qty_by_name[comp_name])
+            comp = comps_by_name[comp_name]
+            qty = qty_by_name[comp_name]
+            row = get_components_parms(mel, next_level, row, comp, qty)
     return row
 
