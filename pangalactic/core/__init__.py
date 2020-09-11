22f946865da4544205255fbdbeca15354212a0b5
Steve Waterbury <waterbug@pangalactic.us>
2020-09-11 12:50:40 -0400

    Bump version to 1.4.0a.

--- pangalactic/core/__init__.py
+++ pangalactic/core/__init__.py
@@ -15,7 +15,7 @@ from sqlalchemy import (Boolean, Date, DateTime, Float, Integer, LargeBinary,
 import ruamel_yaml as yaml
 
 # pangalactic version
-__version__ = '1.4.0'
+__version__ = '1.4.0a'
 
 # `diagramz` is a module-level variable for the diagram cache
 # (see pangalactic.node.gui.diagrams.view for more detail)
