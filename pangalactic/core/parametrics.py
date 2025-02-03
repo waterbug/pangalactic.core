4edc7ffdfa1af2ba7a2b2e6b7d4d116e5aa44090
Stephen Waterbury <waterbug@pangalactic.us>
2025-02-03 15:05:15 -0500

    Fixed recompute_parmz() to avoid "dict changed size during iteration" error.

--- pangalactic/core/parametrics.py
+++ pangalactic/core/parametrics.py
@@ -1255,7 +1255,8 @@ def recompute_parmz():
     # recompute all lower-level component/subassembly values in each call
     # NOTE: a further implication is that non-products (e.g. Port,
     # PortTemplate, etc.) DO NOT HAVE COMPUTED PARAMETERS ...
-    for oid in parameterz:
+    oids = list(parameterz)
+    for oid in oids:
         for variable in variables:
             if variable in parameterz[oid]:
                 for context in d_contexts:
@@ -2391,23 +2392,23 @@ def get_modal_context(project_oid, usage_oid, mode_oid):
         usage_oid (str): the oid of the usage that has the mode
         mode_oid (str): the oid of the mode (activity)
     """
-    log.debug(f'* get_modal_context(project_oid: {project_oid},')
-    log.debug(f'                    usage_oid: {usage_oid},')
-    log.debug(f'                    mode_oid: {mode_oid},')
+    # log.debug(f'* get_modal_context(project_oid: {project_oid},')
+    # log.debug(f'                    usage_oid: {usage_oid},')
+    # log.debug(f'                    mode_oid: {mode_oid},')
     if not project_oid in mode_defz:
-        log.debug('  project_oid was not in mode_defz -- return empty')
+        # log.debug('  project_oid was not in mode_defz -- return empty')
         return ''
     sys_dict = mode_defz[project_oid].get('systems')
     if not sys_dict:
-        log.debug('  no project "systems" found in mode_defz -- return empty')
+        # log.debug('  no project "systems" found in mode_defz -- return empty')
         return ''
     if usage_oid in sys_dict:
         if mode_oid in sys_dict[usage_oid]:
             modal_context = sys_dict[usage_oid][mode_oid]
-            log.debug('  + modal context: {modal_context}')
+            # log.debug('  + modal context: {modal_context}')
             return modal_context
         else:
-            log.debug('  usage_oid in "systems" dict but no mode_oid')
+            # log.debug('  usage_oid in "systems" dict but no mode_oid')
             return ''
     else:
         comp_dict = mode_defz[project_oid].get('components')
@@ -2416,13 +2417,13 @@ def get_modal_context(project_oid, usage_oid, mode_oid):
             for sys_oid in comp_dict:
                 if usage_oid in list(comp_dict[sys_oid]):
                     comp_mode = comp_dict[sys_oid][usage_oid].get(mode_oid)
-                    log.debug(f'  component mode found: {comp_mode}')
+                    # log.debug(f'  component mode found: {comp_mode}')
                     break
-            if not comp_mode:
-                log.debug('  component mode not found: return empty')
+            # if not comp_mode:
+                # log.debug('  component mode not found: return empty')
             return comp_mode or ''
         else:
-            log.debug('  no project "components" found -- return empty')
+            # log.debug('  no project "components" found -- return empty')
             return ''
 
 def set_modal_context(project_oid, sys_usage_oid, usage_oid, mode_oid, level):
@@ -2478,10 +2479,10 @@ def get_modal_power(project_oid, sys_usage_oid, oid, mode, modal_context,
     if modal_context == '[computed]':
         sys_dict = mode_defz[project_oid].get('systems') or {}
         comp_dict = mode_defz[project_oid].get('components') or {}
-        # log.debug('* computing modal power value ...')
+        log.debug('  computing modal power value ...')
         cz = componentz.get(oid)
         if (sys_usage_oid in comp_dict) and cz:
-            # log.debug(f'  - found {len(cz)} components ...')
+            log.debug(f'  - found {len(cz)} components ...')
             # dtype cast is used here in case some component didn't have this
             # parameter or didn't exist and we got a 0.0 value for it ...
             summation = 0.0
@@ -2504,9 +2505,9 @@ def get_modal_power(project_oid, sys_usage_oid, oid, mode, modal_context,
                     else:
                         val = get_pval(c.oid, get_parameter_id('P', context),
                                        units=units)
-                # log.debug(f'    + {val} (context: {context})')
+                log.debug(f'    + {val} (context: {context})')
                 summation += (val or 0.0) * c.quantity
-            # log.debug(f'  total: {summation}')
+            log.debug(f'  total: {summation}')
             return round_to(summation)
         else:
             # if the product has no known components, return its specified
