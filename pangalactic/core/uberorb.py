d9d280d454d71350b209b500583543803b8dca46
Stephen Waterbury <waterbug@pangalactic.us>
2026-07-17 13:48:23 -0400

    Fix orb.gen_rqt_id to use correct "rqt_level" attr (found by claude).

--- pangalactic/core/uberorb.py
+++ pangalactic/core/uberorb.py
@@ -1852,7 +1852,7 @@ class UberORB(object):
         """
         self.log.debug('* generating a new requirement id ...')
         project_id = getattr(reqt.owner, 'id', 'NO-PROJECT')
-        level = getattr(reqt, 'level', 0) or 0
+        level = getattr(reqt, 'rqt_level', 0) or 0
         seq = self.get_next_rqt_seq(reqt.owner, level)
         new_id = project_id + '-' + str(level) + '.' + str(seq)
         self.log.debug(f'  generated id: {new_id}')
