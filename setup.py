964c4e21feb5eb557fb8283537b14d5be0b6f15d
Stephen Waterbury <waterbug@pangalactic.us>
2026-07-26 22:43:59 -0400

    Bump to version 4.4.dev3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ Setup script for pangalactic.core
 import os, site
 from setuptools import setup, find_packages
 
-VERSION = '4.4.dev2'
+VERSION = '4.4.dev3'
 
 onto_mod_path = os.path.join('pangalactic', 'core', 'ontology')
 test_mod_path = os.path.join('pangalactic', 'core', 'test', 'data')
